366003671380c72a7e69d22e00936826ef2bd42b
Vyacheslav Shkalin <nigroborgin@yandex.ru>
2024-05-17 14:00:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# semester-work-androidapp-caresathome
---
+++ app/src/test/java/ru/marsu/semester_work_androidapp_caresathome/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package ru.marsu.semester_work_androidapp_caresathome
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
